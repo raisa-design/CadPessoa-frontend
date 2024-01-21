90ef50e4cf5cdb6239ffe97dddbc793c506e074f
Raisa_Dev <73198136+raisa-design@users.noreply.github.com>
2024-01-21 20:12:48 -0300

    init

---
+++ src/app/conta/models/usuario.ts
@@ -0,0 +1,6 @@
+export interface Usuario {
+    id: string;
+    email: string;
+    senha: string;
+    senhaConfirmacao: string; 
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrlv1: 'https://localhost:7134/api/',
+  imagensUrl: 'https://localhost:7134/api/'
+};
